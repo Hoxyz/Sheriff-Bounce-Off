a3706a51686fe64eca945818290dd08fa9eb609d
Andrija Žarčanin <andrija.zarcanin@gmail.com>
2021-03-06 19:29:54 +0100

    Added player movement script and 8 directional movement animations.

--- Sheriff Bounce-Off/Assets/Scripts/Player.cs
+++ Sheriff Bounce-Off/Assets/Scripts/Player.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float speed = 5f;
+    public Animator animator;
+
+    private Vector2 moveDir;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void FixedUpdate() {
+        rb2d.velocity = moveDir * speed * Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(inputX, inputY).normalized;
+        animator.SetFloat("Horizontal", inputX);
+        animator.SetFloat("Vertical", inputY);
+        animator.SetFloat("Speed", moveDir.SqrMagnitude());
+
+        if (inputX != 0f || inputY != 0f) {
+            animator.SetInteger("LastHorizontal", (int)inputX);
+            animator.SetInteger("LastVertical", (int)inputY);
+        }
+        if (animator.GetInteger("LastHorizontal") == -1) {
+            spriteRenderer.flipX = true;
+        }
+        else {
+            spriteRenderer.flipX = false;
+        }
     }
 }
