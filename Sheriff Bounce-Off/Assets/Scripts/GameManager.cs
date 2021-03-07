e2e5f8eac2476257d120fb1e53d00a0442af8136
Andrija Žarčanin <andrija.zarcanin@gmail.com>
2021-03-07 12:13:47 +0100

    Added colliders on 2d sprites that fake depth, changing players depth while he moves.

---
+++ Sheriff Bounce-Off/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private void Start() {
+        GameObject[] environmentObjects = GameObject.FindGameObjectsWithTag("Environment");
+        foreach (GameObject item in environmentObjects) {
+            Collider2D itemCollider = item.GetComponent<Collider2D>();
+            item.transform.position += new Vector3(0f, 0f, item.transform.position.y + itemCollider.offset.y);
+        }
+    }
+
+}
--- Sheriff Bounce-Off/Assets/Scripts/Player.cs
+++ Sheriff Bounce-Off/Assets/Scripts/Player.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     public float speed = 5f;
     public Animator animator;
+    public GameObject bulletPrefab;
 
+    private Collider2D collider;
     private Vector2 moveDir;
     private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
@@ -14,13 +15,25 @@ public class Player : MonoBehaviour
     private void Start() {
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
     }
     // Update is called once per frame
     void FixedUpdate() {
-        rb2d.velocity = moveDir * speed * Time.deltaTime;
+        if (animator.GetBool("Shooting") == false) {
+            rb2d.velocity = moveDir * speed * Time.deltaTime;
+        }
+        else {
+            rb2d.velocity = Vector2.zero;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y + collider.offset.y);
     }
 
     private void Update() {
+        Move();
+        Shoot();
+    }
+
+    private void Move() {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
 
@@ -40,4 +53,11 @@ public class Player : MonoBehaviour
             spriteRenderer.flipX = false;
         }
     }
+
+    private void Shoot() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            animator.SetBool("Shooting", true);
+        }
+    }
+
 }
